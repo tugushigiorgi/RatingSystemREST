0bfdb261f5515bd4ad91dd3701e5526e0b7710f3
Giorgi Tughushi <giorgi.tughushi@leverx.com>
2025-04-18 16:14:36 +0400

    add logger in EmailServiceimp

--- src/main/java/com/leverx/RatingSystemRest/Business/impl/EmailServiceImp.java
+++ src/main/java/com/leverx/RatingSystemRest/Business/impl/EmailServiceImp.java
@@ -5,18 +5,19 @@ import static com.leverx.RatingSystemRest.Business.ConstMessages.EmailConstMessa
 
 import com.leverx.RatingSystemRest.Business.Interfaces.EmailService;
 import jakarta.mail.MessagingException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-
 /**
- * Service which implement EmailService interface.
+ * Service which implements EmailService interface.
  */
+@Slf4j
 @Service
-
 public class EmailServiceImp implements EmailService {
+
 
   private final JavaMailSender mailSender;
 
@@ -38,28 +39,33 @@ public class EmailServiceImp implements EmailService {
    * @param to    recipient of mail
    * @param token token to send with mail
    */
+  @Override
   public void sendConfirmationEmail(String to, String token) {
+    log.debug("Preparing to send confirmation email to: {}", to);
     try {
       var message = mailSender.createMimeMessage();
       var helper = new MimeMessageHelper(message, true);
       helper.setTo(to);
       helper.setSubject(CONFIRMATION_EMAIL_SUBJECT);
-      helper.setText("Confirmation Code " + token, true);
+      helper.setText("Confirmation Code: " + token, true);
 
       mailSender.send(message);
+      log.info("Confirmation email sent successfully to: {}", to);
 
     } catch (MessagingException e) {
-      e.printStackTrace();
+      log.error("Failed to send confirmation email to: {}", to, e);
     }
   }
 
   /**
-   * Send Recover link.
+   * Send recovery link.
    *
    * @param email recipient of mail
    * @param token token to send
    */
+  @Override
   public void sendRecoverLink(String email, String token) {
+    log.debug("Preparing to send recovery email to: {}", email);
     try {
       var message = mailSender.createMimeMessage();
       var helper = new MimeMessageHelper(message, true);
@@ -67,9 +73,10 @@ public class EmailServiceImp implements EmailService {
       helper.setSubject(RECOVERY_EMAIL_SUBJECT);
       helper.setText(frontendPath + "reset" + "?token=" + token);
       mailSender.send(message);
+      log.info("Recovery email sent successfully to: {}", email);
 
     } catch (MessagingException e) {
-      e.printStackTrace();
+      log.error("Failed to send recovery email to: {}", email, e);
     }
   }
 }
